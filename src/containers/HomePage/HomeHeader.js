7ba73174d518d231e2aba695d369da071407d1c3
Hieu Pham <hieu02617@gmail.com>
2025-02-18 14:13:59 +0700

    #44

---
+++ src/containers/HomePage/HomeHeader.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./HomeHeader.scss";
+
+class HomeHeader extends Component {
+  render() {
+    return (
+      <div className="home-header-container">
+        <div className="home-header-content">
+          <div className="left-content">
+            <i className="fas fa-bars icon-bars"></i>
+            <div className="header-logo"></div>
+          </div>
+          <div className="center-content">
+            <div className="child-content">
+              <div className="child-content-heading">
+                <b>Chuyên khoa</b>
+              </div>
+              <div className="child-content-subheading">
+                Tìm bác sĩ theo chuyên khoa
+              </div>
+            </div>
+
+            <div className="child-content">
+              <div className="child-content-heading">
+                <b>Cơ sở y tế</b>
+              </div>
+              <div className="child-content-subheading">
+                Chọn bệnh viện phòng khám
+              </div>
+            </div>
+
+            <div className="child-content">
+              <div className="child-content-heading">
+                <b>Bác sĩ</b>
+              </div>
+              <div className="child-content-subheading">Chọn bác sĩ giỏi</div>
+            </div>
+
+            <div className="child-content">
+              <div className="child-content-heading">
+                <b>Gói khám</b>
+              </div>
+              <div className="child-content-subheading">
+                Khám sức khỏe tổng quát
+              </div>
+            </div>
+          </div>
+          <div className="right-content">
+            <div className="support">
+              <i className="fas fa-question-circle icon-support"></i> Hỗ trợ
+            </div>
+            <div className="flag">VN</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
